51b48f1ef6ed5a9ee5b9d9beb972e95fb963cc84
Vinay Kumar <vinayk1427@gmail.com>
2025-11-23 13:39:58 +0530

    commit changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[flag,setFlag] = useState(false);
   return (
     <div id="main">
+      <button id="click" onClick={()=>setFlag(true)}>Click</button>
+      {flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
       // Do not alter the main div
     </div>
   );
